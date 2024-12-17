d8d23d61d8346fd34f90f0f199085f6c7464882e
Hwang20241024 <ghkdaksdbs@naver.com>
2024-12-17 20:38:04 +0900

    feat: socketUser클래스 생성

--- src/handlers/stage.manager.handler.js
+++ src/handlers/stage.manager.handler.js
@@ -1,76 +1,11 @@
 // 맵핑된 핸들러 연결
 import handlerMappings from './handlerMapping.js';
 
-
-// 소캣 사용자 정보 저장용.
-class StageManager {
-  constructor(socket) {
-    this.socket = socket;
+import SocketUser from '../models/sessions/socketUser.js';
 
 
-    // 각종 정보를 저장해 놓자.
-    this.userInfo = {
-      uuid: null,
-      health: 3,
-      speed: 1,
-      current_info: {
-        stage: null,
-        score: null,
-      },
-      best_info: {
-        stage: null,
-        score: null,
-      },
-      x: null,
-      y: null,
-    };
-  }
-
-  // 유저 초기화
-  initializeUser(info) {
-    this.userInfo = {
-      uuid: info.uuid,
-      health: info.health,
-      speed: info.speed,
-      current_info: {
-        stage: info.current_info.stage,
-        score: info.current_info.score,
-      },
-      best_info: {
-        stage: info.best_info.stage,
-        score: info.best_info.score,
-      },
-      x: info.x,
-      y: info.y,
-    };
-  }
-
-  // 스테이지 전환
-  switchStage(stage) {
-    switch (stage) {
-      case 1:
-        this.userInfo.current_info.stage = 0;
-        break;
-      case 2:
-        this.userInfo.current_info.stage = 1;
-        break;
-      case 3:
-        this.userInfo.current_info.stage = 2;
-        break;
-      case 4:
-        this.userInfo.current_info.stage = 3;
-        break;
-      case 5:
-        this.userInfo.current_info.stage = 4;
-        break;
-      case 6:
-        this.userInfo.current_info.stage = 5;
-        break;
-    }
-  }
-}
-
-let connectedSocketsCount = 0; // 연결된 소켓의 수를 추적하는 변수
+// 연결된 소켓의 수를 추적하는 변수
+let connectedSocketsCount = 0; 
 
 const stageHandler = (io) => {
   io.on('connection', async (socket) => {
@@ -79,11 +14,11 @@ const stageHandler = (io) => {
     // 서버에 총 접속인원.
     connectedSocketsCount++;
 
-    // 스테이지 매니저 인스턴스 생성
-    const stageManager = new StageManager(socket);
+    // 유저 정보 추가.
+    const socketUser = new SocketUser(socket);
+    // 현재 스테이지 지정 (1은 메인화면 )
+    socketUser.currentStage = 1;
 
-    // 초기에는 메인스테이지 시작
-    stageManager.switchStage(1);
 
     socket.emit('connected', '게임에 접속하신걸 환영합니다.'); // 클라이언트로 응답 보내기
     socket.emit('connected', '닉네임을 입력하시고 버튼을 누르면 게임을 실행합니다.');
@@ -91,8 +26,8 @@ const stageHandler = (io) => {
     // 게임로그 .
     const gameLog = handlerMappings[4];
     if(gameLog) {
-      gameLog(io, stageManager.socket, 0, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
-      gameLog(io, stageManager.socket, 1, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
+      gameLog(io, socketUser.socket, 0, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
+      gameLog(io, socketUser.socket, 1, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
     }
 
 
@@ -100,7 +35,7 @@ const stageHandler = (io) => {
     const cumulativeRankings = handlerMappings[2];
     const rankings = handlerMappings[6]; 
     if(cumulativeRankings && rankings) {
-      rankings(io, stageManager.socket, 'cumulativeRankings', await cumulativeRankings());
+      rankings(io, socketUser.socket, 'cumulativeRankings', await cumulativeRankings());
     }
     
 
@@ -112,17 +47,17 @@ const stageHandler = (io) => {
       
       if(nicknameEvent && mainUserInfo){
         // 이미 존재한다면 기존 정보를 db에서 가져온다.
-        const info = await nicknameEvent(data, stageManager.userInfo);
-        stageManager.initializeUser(info);
+        const info = await nicknameEvent(data, socketUser.userInfo);
+        socketUser.initializeUser(info);
 
         // 생성된 정보를 유저에게 보내자.
-        mainUserInfo(stageManager.socket, info, data.payload.message);
+        mainUserInfo(socketUser.socket, info, data.payload.message);
       }
 
       // 1-2. 스테이지 체인지.
       const seneChange = handlerMappings[7];
       if(seneChange){
-        stageManager.switchStage(2);
+        socketUser.currentStage = 2;
       }
       
     });
@@ -140,8 +75,8 @@ const stageHandler = (io) => {
     // 클라이언트에서 접속헤제 했을때 이벤트
     socket.on('disconnect', () => {
       connectedSocketsCount--;
-      gameLog(io, stageManager.socket, 0, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
-      gameLog(io, stageManager.socket, 1, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
+      gameLog(io, socketUser.socket, 0, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
+      gameLog(io, socketUser.socket, 1, `현재 접속인원은 ${connectedSocketsCount - 1}명입니다.`);
     });
   });
 };
---
+++ src/models/sessions/socketUser.js
@@ -0,0 +1,158 @@
+// 소캣 사용자 정보 저장용.
+export default class SocketUser {
+  constructor(socket) {
+    this._socket = socket;
+    this._name = null;
+    this._keyInput = null; 
+    this._isjump = false;
+
+    // 각종 정보를 저장해 놓자.
+    this._userInfo = {
+      uuid: null,
+      health: 3,
+      speed: 1,
+      current_info: {
+        stage: null,
+        score: null,
+      },
+      best_info: {
+        stage: null,
+        score: null,
+      },
+      x: null,
+      y: null,
+    };
+  }
+
+  /*====[ 유저 초기화 ]====*/
+  initializeUser(info) {
+    this._userInfo = {
+      uuid: info.uuid,
+      health: info.health,
+      speed: info.speed,
+      current_info: {
+        stage: info.current_info.stage,
+        score: info.current_info.score,
+      },
+      best_info: {
+        stage: info.best_info.stage,
+        score: info.best_info.score,
+      },
+      x: info.x,
+      y: info.y,
+    };
+  }
+  /*==================*/
+
+  /*====[ GET 함수 ]====*/
+
+  // (GET) 유저 정보.
+  get socket() {
+    return this._socket;
+  }
+
+  // (GET) 유저 정보.
+  get userInfo() {
+    return this._userInfo;
+  }
+
+  // (GET) uuid.
+  get uuid() {
+    return this._userInfo.uuid;
+  }
+
+  // (GET) 체력.
+  get health() {
+    return this._userInfo.health;
+  }
+
+  // (GET) 스피드.
+  get speed() {
+    return this._userInfo.speed;
+  }
+  
+  // (GET) 스테이지.
+  get currentStage() {
+    return this._userInfo.current_info.stage;
+  }
+
+  // (GET) 점수.
+  get currentScore() {
+    return this._userInfo.current_info.score;
+  }
+
+  // (GET) 좌표.
+  get position() {
+    const position = {
+      x : this._userInfo.x,
+      y : this._userInfo.y
+    }
+    return position;
+  }
+
+  // (GET) 이름.
+  get name() {
+    return this._name;
+  }
+
+  // (GET) 키입력.
+  get keyInput() {
+    return this._keyInput;
+  }
+
+  // (GET) 점프확인.
+  get isjump() {
+    return this._isjump;
+  }
+  /*==================*/
+  
+
+  /*====[ SET 함수 ]====*/
+  // (SET) uuid.
+  set uuid(value) {
+    this._userInfo.uuid = value;
+  }
+
+  // (SET) 체력.
+  set health(value) {
+    this._userInfo.health = value;
+  }
+
+  // (SET) 스피드.
+  set speed(value) {
+    this._userInfo.speed = value;
+  }
+  
+  // (SET) 스테이지.
+  set currentStage(value) {
+    this._userInfo.current_info.stage = value;
+  }
+
+  // (SET) 점수.
+  set currentScore(value) {
+    this._userInfo.current_info.score = value;
+  }
+
+  // (SET) 좌표.
+  set position(value) {
+    this._userInfo.x = value.x;
+    this._userInfo.y = value.y;
+  }
+
+  // (SET) 이름.
+  set name(value) {
+    this._name = value;
+  }
+
+  // (SET) 키입력.
+  set keyInput(value) {
+    this._keyInput = value;
+  }
+
+  // (SET) 점프확인.
+  set isjump(value) {
+    this._isjump = value;
+  }
+  /*==================*/
+  
+}
